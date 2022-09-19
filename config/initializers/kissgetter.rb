9f980d33c289719a2337df032da1b58bea5aae56
Meerkat81 <cmeurer@ionizationlabs.com>
2022-09-18 20:10:12 -0500

    initial commit

---
+++ config/initializers/kissgetter.rb
@@ -0,0 +1,7 @@
+response = HTTParty.get('https://lsp-prod.cmg.com/api/v3/histories/kissrocks.com/')
+
+puts "plerere", Artist.last
+# scheduler = Rufus::Scheduler.new
+# scheduler.every '3s' do
+#   	puts 'Hello... Rufus'
+# end
